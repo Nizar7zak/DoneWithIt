554b4713ec7020c9ae927ada74932178fff52db5
Nizar7zak <nizar7zakout@gmail.com>
2022-01-02 02:29:03 +0200

    errorMessage Component

---
+++ app/components/ErrorMessage.js
@@ -0,0 +1,10 @@
+import { AppText } from "./AppText";
+
+export const ErrorMessage = ({ error }) => {
+  if (!error) return null;
+  return <AppText style={styles.error}>{error}</AppText>;
+};
+
+const styles = StyleSheet.create({
+  error: { color: "red" },
+});
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -7,6 +7,7 @@ import { AppImage } from "../components/AppImage";
 import { AppTextInput } from "../components/AppTextInput";
 import { AppButton } from "../components/AppButton";
 import { AppText } from "../components/AppText";
+import { ErrorMessage } from "../components/ErrorMessage";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -30,7 +31,6 @@ const Login = () => {
         initialValues={{ email: "", password: "" }}
         validationSchema={validationSchema}
         onSubmit={(values) => console.log(values)}
-        
       >
         {({ handleChange, handleSubmit, errors }) => (
           <>
@@ -43,7 +43,7 @@ const Login = () => {
               textContentType="emailAddress"
               onChangeText={handleChange("email")}
             />
-            <AppText style={{color:"red"}}>{errors.email}</AppText>
+            <ErrorMessage error={errors.email} />
             <AppTextInput
               placeholder="Password"
               iconName="lock"
@@ -53,7 +53,8 @@ const Login = () => {
               secureTextEntry
               onChangeText={handleChange("password")}
             />
-            <AppText style={{color:"red"}}>{errors.password}</AppText>
+            <ErrorMessage error={errors.password} />
+
             <AppButton title="login" onPress={handleSubmit} />
           </>
         )}
