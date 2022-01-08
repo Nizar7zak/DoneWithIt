f68e81d68f3ac1380536d1e65d161168c90b50b2
Nizar7zak <nizar7zakout@gmail.com>
2022-01-08 20:57:08 +0200

    calling api using api sauce for listings data

--- app/screens/Listing.js
+++ app/screens/Listing.js
@@ -2,21 +2,34 @@ import { StyleSheet, FlatList } from "react-native";
 
 import { SafeScreen } from "../components/SafeScreen";
 import { Card } from "../components/Card";
-import fakeData from "../project/data/fakeData";
+// import fakeData from "../project/data/fakeData";
+import apiListings from "../api/lisitings";
 import routes from "../components/router/routes";
+import { useEffect, useState } from "react";
 
 const Lisitng = ({ navigation }) => {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const { data } = await apiListings.getListings();
+    setListings(data);
+  };
+
   return (
     <SafeScreen style={styles.container}>
       <FlatList
-        data={fakeData}
+        data={listings}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <Card
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            title={item.name}
+            title={item.title}
             subTitle={`${item.price} $`}
-            imagePath={item.image}
+            imagePath={{uri : item.images[0].url}}
           />
         )}
       />
