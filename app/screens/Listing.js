63830306a7e0efa85a33c731270c979f003bf8b8
Nizar7zak <nizar7zakout@gmail.com>
2022-01-09 03:34:46 +0200

    handling error on listing screen

--- app/screens/Listing.js
+++ app/screens/Listing.js
@@ -1,26 +1,36 @@
 import { StyleSheet, FlatList } from "react-native";
+import { useEffect, useState } from "react";
 
+import { AppText } from "../components/AppText";
+import { AppButton } from "../components/AppButton";
 import { SafeScreen } from "../components/SafeScreen";
 import { Card } from "../components/Card";
-// import fakeData from "../project/data/fakeData";
 import apiListings from "../api/lisitings";
 import routes from "../components/router/routes";
-import { useEffect, useState } from "react";
 
 const Lisitng = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
-    const { data } = await apiListings.getListings();
-    setListings(data);
+    const response = await apiListings.getListings();
+    if (!response.ok) return setError(true);
+    setError(false);
+    setListings(response.data);
   };
 
   return (
     <SafeScreen style={styles.container}>
+      {error && (
+        <>
+          <AppText>Couldn't retrive the lisitings</AppText>
+          <AppButton title="Retry" onPress={() => loadListings()} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(item) => item.id.toString()}
@@ -29,7 +39,7 @@ const Lisitng = ({ navigation }) => {
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
             title={item.title}
             subTitle={`${item.price} $`}
-            imagePath={{uri : item.images[0].url}}
+            imagePath={{ uri: item.images[0].url }}
           />
         )}
       />
