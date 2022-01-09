312b69e6a3985e76dee8ca926d6ab14bd95387b7
Nizar7zak <nizar7zakout@gmail.com>
2022-01-09 05:36:04 +0200

    extending api layer addlisting -POST

--- app/api/lisitings.js
+++ app/api/lisitings.js
@@ -4,6 +4,27 @@ const endPoint = "/listings";
 
 const getListings = () => client.get(endPoint);
 
+const addListing = (lisitng) => {
+  const data = new FormData();
+  data.append("title", lisitng.title);
+  data.append("price", lisitng.price);
+  data.append("categoryId", lisitng.category.value);
+  data.append("description", lisitng.description);
+
+  lisitng.image.forEach((photo, index) => {
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: photo,
+    });
+  });
+  if (lisitng.location)
+    data.append("location", JSON.stringify(lisitng.location));
+
+  return client.post(endPoint, data);
+};
+
 export default {
   getListings,
+  addListing,
 };
