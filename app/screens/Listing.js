7b47d8aa1d5e58ae3f92ad27fb5e28fa9fbed0b7
Nizar7zak <nizar7zakout@gmail.com>
2022-01-09 03:41:39 +0200

    handle slow network with activity indicator on listing screen

--- app/screens/Listing.js
+++ app/screens/Listing.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 import { AppText } from "../components/AppText";
 import { AppButton } from "../components/AppButton";
+import { LoadingScreen } from "../components/Loading";
 import { SafeScreen } from "../components/SafeScreen";
 import { Card } from "../components/Card";
 import apiListings from "../api/lisitings";
@@ -11,14 +12,19 @@ import routes from "../components/router/routes";
 const Lisitng = ({ navigation }) => {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await apiListings.getListings();
+    setLoading(false);
+
     if (!response.ok) return setError(true);
+
     setError(false);
     setListings(response.data);
   };
@@ -31,6 +37,7 @@ const Lisitng = ({ navigation }) => {
           <AppButton title="Retry" onPress={() => loadListings()} />
         </>
       )}
+      {loading && <LoadingScreen />}
       <FlatList
         data={listings}
         keyExtractor={(item) => item.id.toString()}
